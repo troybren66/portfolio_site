978af8e2d41d73417b5c9a41616feeb7b9c12ba0
Troy Brennan <troybrennan@Troys-MacBook-Air.local>
2020-06-11 17:57:26 -0400

    Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {Titleh1, TitleContainer} from './styled'
+
+
+
+export default function Title() {
+    return (
+        <TitleContainer>
+            <Titleh1>Troy</Titleh1>
+        </TitleContainer>
+    )
+}
+
+
+
+
---
+++ src/components/Title/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+
+// Create a Title component that'll render an <h1> tag with some styles
+
+export const Titleh1 = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: red;
+  font-family:'TRON';font-weight:normal;font-size:85px
+`;
+
+export const TitleContainer = styled.div`
+height: 100vh;
+background:black;
+background-size: 40px 40px;
+    background-image:
+      linear-gradient(to right, #01D5DA 1px, transparent 1px),
+      linear-gradient(to bottom,  #01D5DA 1px, transparent 1px);
+
+`
